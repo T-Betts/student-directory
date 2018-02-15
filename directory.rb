ca879270a12bdaffef088b3971db74e6d77cd81a
Tom Betts <tom.betts@live.co.uk>
2018-02-15 16:09:20 +0000

    Add interactive menu method to directory with first few options

--- directory.rb
+++ directory.rb
@@ -1,8 +1,8 @@
 def input_students
   puts "Please enter the names of the students."
   puts "To finish, just hit enter twice."
-  students = []
   name = gets.chomp
+  students = []
   until name == ""
     students << {name: name, cohort: :november}
     puts "Now we have #{students.count} students."
@@ -26,7 +26,26 @@ def print_footer(student_arr)
   puts "Overall we have #{student_arr.count} great students"
 end
 
-students = input_students
-print_header
-print(students)
-print_footer(students)
+def interactive_menu
+  students = []
+  loop do
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit"
+    selection = gets.chomp
+    case selection
+    when "1"
+      students = input_students
+    when "2"
+      print_header
+      print(students)
+      print_footer(students)
+    when "9"
+      exit
+    else
+      puts "Sorry, I didn't understand that instruction, please try again."
+    end
+  end
+end
+
+interactive_menu
