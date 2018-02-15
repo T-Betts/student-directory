28134d627a8cefa6fe98239f8507d5e2074040a5
Tom Betts <tom.betts@live.co.uk>
2018-02-15 15:23:59 +0000

    Switch from hardcoding student array to asking for user input

--- directory.rb
+++ directory.rb
@@ -1,17 +1,15 @@
-# students in an array
-students = [
-  {name: "Dr. Hannibal Lecter", cohort: :novemeber},
-  {name: "Darth Vader", cohort: :november},
-  {name: "Nurse Ratched", cohort: :november},
-  {name: "Michael Corleone", cohort: :november},
-  {name: "Alex DeLarge", cohort: :november},
-  {name: "The Wicked Witch of the West", cohort: :november},
-  {name: "Terminator", cohort: :november},
-  {name: "Freddy Krueger", cohort: :november},
-  {name: "The Joker", cohort: :november},
-  {name: "Joffrey Baratheon", cohort: :november},
-  {name: "Norman Bates", cohort: :november}
-]
+def input_students
+  puts "Please enter the names of the students."
+  puts "To finish, just hit enter twice."
+  students = []
+  name = gets.chomp
+  until name == ""
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students."
+    name = gets.chomp
+  end
+  students
+end
 
 def print_header
   puts "The students of Villains Academy"
@@ -28,6 +26,7 @@ def print_footer(student_arr)
   puts "Overall we have #{student_arr.count} great students"
 end
 
+students = input_students
 print_header
 print(students)
 print_footer(students)
